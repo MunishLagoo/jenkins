a4c1e8d8b716c373e4a6fa2eab3c535085fb0598
Munish Lagoo <munishlagoo@gmail.com>
2021-12-20 21:22:24 -0500

    add nodejs.groovy Code

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,5 +1,5 @@
 def call(Map params = [:]) {
-    def args :LinkedHashMap = [
+    def args LinkedHashMap = [
         COMPONENT: ''
         LABEL : 'WORKSTATION'
     ] 
@@ -7,6 +7,16 @@ def call(Map params = [:]) {
     pipeline {
         agent {label params.LABEL}
         stages {
+            stage('Labeling Build') {
+                steps {
+                    script {
+                      str = GIT_BRANCH.split('/').last()
+                      addshortText background: 'yellow', color: 'black', borderColor:'yellow', text:"COMPONENT=${params.COMPONENT}"
+                      //addshortText background: 'yellow', color: 'black', borderColor:'yellow', text:"APP_VERSION=${params.APP_VERSION}"
+                      addshortText background: 'yellow', color: 'black', borderColor:'yellow', text:"BRANCH=${str}"
+                    }
+                }
+            }
             stage('Compile') {
                 steps {
                     sh "echo COMPONENT = ${params.COMPONENT}"
@@ -24,6 +34,11 @@ def call(Map params = [:]) {
                 }
             }
              stage('Upload Artifacts') {
+                 when {
+                     expression {
+                         sh ([returnstdout: true, script:'echo ${GIT_BRANCH}| grep tags || true'])
+                         }
+                 }
                 steps {
                     sh 'echo Test-Cases'
                 }
