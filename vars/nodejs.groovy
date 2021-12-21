9ab645a204ac61646fb58ba60bb15d57fe26b930
Munish Lagoo <munishlagoo@gmail.com>
2021-12-21 13:44:27 -0500

    CI Add Nexus

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -63,6 +63,7 @@ def call(Map params = [:]) {
                 steps {                    
                     sh """
                        GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
+                       echo \${GIT_TAG}
                        echo \${GIT_TAG} > version
                        zip -r ${params.COMPONENT}-\${GIT_TAG}.zip node_modules server.js version
                        echo "uploading artifacts to Nexus"
