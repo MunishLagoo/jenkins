0af02b3b3706be5caaf73096167fb7a4e118f3fa
Munish Lagoo <munishlagoo@gmail.com>
2021-12-21 07:12:08 -0500

    attached seed job for CI

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,12 +1,33 @@
-def call() {
+def call(Map params = [:]) {
+    def args = [
+        COMPONENT: '',
+        LABEL : 'WORKSTATION'
+    ] 
+    args<< params
     pipeline {
-        agent any
+        agent {label params.LABEL}
         stages {
-            stage('Compile') {
+            stage('Labeling Build') {
                 steps {
-                    sh 'echo Compile'
+                    script {
+                      str = GIT_BRANCH.split('/').last()
+                       addShortText background: 'yellow', color: 'black', borderColor:'yellow', text:"COMPONENT=${params.COMPONENT}"
+            //           addShortText background: 'yellow', color: 'black', borderColor:'yellow', text:"APP_VERSION=${params.APP_VERSION}"
+                       addShortText background: 'yellow', color: 'black', borderColor:'yellow', text:"BRANCH=${str}"
+                    }
                 }
             }
+            // stage('Download NodeJS Dependencies') {
+            //     steps {
+            //         sh """
+            //         echo "+++ Before"
+            //         ls -l
+            //         npm install
+            //         echo "+++ after"
+            //         ls -l
+            //         """
+            //     }
+            // }
             stage('Code Quality') {
                 steps {
                     sh 'echo Quality'
@@ -17,6 +38,21 @@ def call() {
                     sh 'echo Test-Cases'
                 }
             }
+             stage('Upload Artifacts') {
+                 when {
+                     expression {
+                         sh ([returnStdout: true, script:'echo ${GIT_BRANCH} | grep tags || true'])
+                         }
+                 }
+                steps {
+                    sh 'echo Test-Cases'
+                }
+            }
         }
+      post {
+          always {
+              cleanWs()
+          }
+      }  
     }
 }
