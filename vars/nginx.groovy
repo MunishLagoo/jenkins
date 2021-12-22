488c33fcafa6c64afda2519febbf2f17aa49d9e6
Munish Lagoo <munishlagoo@gmail.com>
2021-12-22 07:50:19 -0500

    upload

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -42,7 +42,7 @@ def call(Map params = [:]) {
                        GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
                        echo \${GIT_TAG} > version
                        cd static
-                       zip -r ${params.COMPONENT}-\${GIT_TAG}.zip *
+                       zip -r ../${params.COMPONENT}-\${GIT_TAG}.zip *
                        cd ..
                        echo "uploading artifacts to Nexus"
                        curl -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\${GIT_TAG}.zip http://172.31.10.172:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\${GIT_TAG}.zip
