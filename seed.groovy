2d7240c4d0650a9af4959d892582f5790e6a08a8
Munish Lagoo <munishlagoo@gmail.com>
2022-01-11 09:11:31 -0500

    Add helm values

--- seed.groovy
+++ seed.groovy
@@ -154,3 +154,26 @@ pipelineJob('Helm-Deploy') {
  }
 }
 
+
+pipelineJob('k8s-DB-Deploy') {
+ configure { flowdefinition ->
+   flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+     'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+       'userRemoteConfigs' {
+         'hudson.plugins.git.UserRemoteConfig' {
+           'url'("https://github.com/MunishLagoo/jenkins.git")
+           //'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/frontend')
+         }
+       }
+       'branches' {
+         'hudson.plugins.git.BranchSpec' {
+           'name'('*/main')
+         }
+       }
+     }
+     'scriptPath'('Jenkinsfile-all-components')
+     'lightweight'(true)
+   }
+ }
+}
+
